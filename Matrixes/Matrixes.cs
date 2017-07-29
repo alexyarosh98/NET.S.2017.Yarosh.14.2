3f958445455b49368969867555790e8ff4795df2
Alexander <alexyarosh98@gmail.com>
2017-07-29 14:48:21 +0300

    Добавьте файлы проекта.

---
+++ Matrixes/Matrixes.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrixes
+{
+    public abstract class Matrixes<T>
+    {
+        private int dimension;
+        protected internal T[,] matrix;
+
+        public int Dimension
+        {
+            get { return dimension; }
+        }
+
+        public EventHandler ChangeHandler;
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="n">dimmension of matrix</param>
+        protected Matrixes(int n)
+        {
+            if (n <= 0) throw new ArgumentOutOfRangeException($"{nameof(n)} must be a positive number");
+
+            dimension = n;
+            matrix = new T[dimension, dimension];
+        }
+        /// <summary>
+        /// Change element on postition (i,j)
+        /// </summary>
+        /// <param name="i">line postition of element to be chanched</param>
+        /// <param name="j">column position of element to be changed</param>
+        /// <param name="obj">new element</param>
+        /// <exception cref="ArgumentOutOfRangeException">i and j must be greater then 0 but less then dimension of matrix</exception>
+        /// <exception cref="ArgumentNullException">new element must not be null</exception>
+        public virtual void ChangeElement(int i, int j, T obj)
+        {
+            if(i<0||j<0||j>=Dimension||i>=Dimension) throw new ArgumentOutOfRangeException
+                    ($"{nameof(i)} and {nameof(j)} must be from 0 to {Dimension-1}");
+            if(ReferenceEquals(obj,null)) throw new ArgumentNullException($"{nameof(obj)} must not be null");
+
+            matrix[i, j] = obj;
+
+            MatrixChanged(this,new EventArgs());
+        }
+
+        #region Addition
+        /// <summary>
+        /// Add matrix to this one
+        /// </summary>
+        /// <param name="obj">matrix to be added</param>
+        /// <param name="additionFunc">logic of addition 2 elements in matrixes</param>
+        /// <exception cref="ArgumentNullException">parametres must not be null</exception>
+        /// <exception cref="InvalidOperationException">type of elements in both matrixes must be the same</exception>
+        /// <returns>this matrix(changed) if types are the same and new SquareMatrix if types are different</returns>
+        public virtual Matrixes<T> AddMatrixes(Matrixes<T> obj, Func<T, T, T> additionFunc)
+        {
+            if (ReferenceEquals(obj, null)) throw new ArgumentNullException($"{nameof(obj)} must not be null");
+            if(ReferenceEquals(additionFunc,null)) throw new ArgumentNullException($"{nameof(additionFunc)} must not be null");
+            if (matrix.GetType() != obj.matrix.GetType())
+                throw new InvalidOperationException("Type of elements in matrixes must be the same");
+
+
+            if (this.GetType() == obj.GetType())
+            {
+                this.AddMatrix(obj, additionFunc);
+                return this;
+            }
+
+            SquareMatrix<T> newMatrix = new SquareMatrix<T>(Math.Max(obj.Dimension, Dimension));
+            for (int i = 0; i < newMatrix.Dimension; i++)
+            {
+                for (int j = 0; j < newMatrix.Dimension; j++)
+                {
+                    if (this.Dimension >= obj.Dimension)
+                    {
+                        newMatrix.matrix[i, j] = matrix[i, j];
+                    }
+                    else
+                    {
+                        newMatrix.matrix[i, j] = obj.matrix[i, j];
+                    }
+                }
+            }
+
+            if (this.Dimension >= obj.Dimension)
+            {
+                newMatrix.AddMatrix(obj, additionFunc);
+            }
+            else
+            {
+                newMatrix.AddMatrix(this, additionFunc);
+            }
+
+            return newMatrix;
+
+        }
+        private  void AddMatrix(Matrixes<T> obj, Func<T, T, T>additionFunc)
+        {
+            if (ReferenceEquals(obj, null)) throw new ArgumentNullException($"{nameof(obj)} must not be null");
+            if (matrix.GetType() != obj.matrix.GetType())
+                throw new InvalidOperationException("Type of elements in matrixes must be the same");
+            
+            int maxDim = Math.Max(obj.Dimension, Dimension);
+            int minDim = Math.Min(obj.Dimension, Dimension);
+
+            T[,] newMatrix = new T[maxDim, maxDim];
+
+            for (int i = 0; i < minDim; i++)
+            {
+                for (int j = 0; j < minDim; j++)
+                {
+                    newMatrix[i, j] = additionFunc(matrix[i, j], obj.matrix[i, j]);
+                }
+            }
+
+            T[,] arr = null;
+            if (obj.Dimension > Dimension) arr = obj.matrix;
+            else if (obj.Dimension < Dimension) arr = this.matrix;
+
+            if (arr != null)
+            {
+                for (int i = 0; i < maxDim; i++)
+                {
+                    for (int j = minDim; j < maxDim; j++)
+                    {
+                        newMatrix[i, j] = arr[i, j];
+                    }
+                }
+                for (int i = minDim; i < maxDim; i++)
+                {
+                    for (int j = 0; j < maxDim; j++)
+                    {
+                        newMatrix[i, j] = arr[i, j];
+                    }
+                }
+            }
+
+            matrix = newMatrix;
+            dimension = maxDim;
+        }
+
+
+
+        #endregion
+
+        private void MatrixChanged(object o, EventArgs args)
+        {
+            ChangeHandler(o, args);
+        }
+    }
+}
---
+++ Matrixes/SpecialMatrixes.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Matrixes
+{
+    public class SymmetricalMatrix<T>:Matrixes<T>
+    {
+    /// <summary>
+    /// create new symmetrical matrix 
+    /// </summary>
+    /// <param name="n">dimension of matrix</param>
+    /// <param name="elems">half of elements in matrix</param>
+        public SymmetricalMatrix(int n,IEnumerable<T> elems):base(n)
+        {
+           if(n<=0) throw new ArgumentOutOfRangeException($"{nameof(n)} must be a positive number");
+           if(ReferenceEquals(elems,null)) throw new ArgumentNullException($"{nameof(elems)} must not be null");
+           if(elems.Count()!=NumberOfExpectedElems(n)) throw new ArgumentException
+                    ($"Numaer of elements in {nameof(elems)} must be {NumberOfExpectedElems(n)}");
+
+            int index = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    matrix[i, j] = elems.ElementAt(index);
+                    matrix[j, i] = elems.ElementAt(index++);
+                }
+            }
+        }
+        /// <summary>
+        /// create new empty symmetrical matrix
+        /// </summary>
+        /// <param name="n">dimension of matrix</param>
+        public SymmetricalMatrix(int n):base(n)
+        {
+            
+        }
+        /// <summary>
+        /// Change element on position (i,j) and (j,i) 
+        /// </summary>
+        /// <param name="i">line position of element</param>
+        /// <param name="j">column position of eleemnt</param>
+        /// <exception cref="ArgumentNullException">new element must not be null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">i and j must be greater then 0 and less then dimension of matrix</exception>
+        /// <param name="obj">new element</param>
+        public override void ChangeElement(int i, int j, T obj)
+        {
+            base.ChangeElement(j,i,obj);
+            base.ChangeElement(i, j, obj);
+        }
+
+        private int NumberOfExpectedElems(int n)
+        {
+            int res = 0;
+            for (int i = 1; i < n; i++)
+            {
+                res += i;
+            }
+
+            return res;
+        }
+    }
+
+    public class SquareMatrix<T>:Matrixes<T>
+    {
+        /// <summary>
+        /// create new Square matrix 
+        /// </summary>
+        /// <param name="n">dimension of matrix</param>
+        /// <param name="elems">elements of matrix</param>
+        /// <exception cref="ArgumentNullException">elements must nit be null</exception>
+        /// <exception cref="ArgumentException">Number of elements must be the same as number of elements in square matrix</exception>
+        public SquareMatrix(int n,IEnumerable<T> elems):base(n)
+        {
+            if(ReferenceEquals(elems,null)) throw new ArgumentNullException($"{nameof(elems)} must be not null");
+            if(elems.Count()!=n*n) throw new ArgumentException($"Wrong number of elements in {nameof(elems)}." +
+                                                               $"\n Number of elements with this dimensions must be {n*n}");
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = elems.ElementAt(index++);   
+                }
+            }
+        }
+        /// <summary>
+        /// create empty square matrix
+        /// </summary>
+        /// <param name="n">dimension of matrix</param>
+        public SquareMatrix(int n):base(n)
+        {
+            
+        }
+        
+    }
+
+    public class DiagonalMatrix<T>:Matrixes<T>
+    {
+        /// <summary>
+        /// create new Diagonal matrix
+        /// </summary>
+        /// <param name="n">dimension of matrix</param>
+        /// <param name="elems">elements on main diagonal</param>
+        /// <exception cref="ArgumentNullException">elemets must not be null</exception>
+        /// <exception cref="ArgumentException">Number of elements must be the same as number of element on main diagonal in matrix</exception>
+        public DiagonalMatrix(int n,IEnumerable<T> elems):base(n)
+        {
+            if(ReferenceEquals(elems,null)) throw new ArgumentNullException($"{nameof(elems)} must not be null");
+            if (elems.Count() != n) throw new ArgumentException($"{nameof(elems)} must consist of {n} elements");
+
+            for (int i = 0, j = 0; i < n; i++, j++)
+            {
+                matrix[i, j] = elems.ElementAt(i);
+            }
+        }
+        /// <summary>
+        /// create empty diagonal matrix
+        /// </summary>
+        /// <param name="n">dimension of matrix</param>
+        public DiagonalMatrix(int n):base(n)
+        {
+            
+        }
+        /// <summary>
+        /// chanche element on main diagonal
+        /// </summary>
+        /// <param name="i">line position of element</param>
+        /// <param name="j">column position of element</param>
+        /// <param name="obj">new element</param>
+        /// <exception cref="ArgumentNullException">new element must not be null</exception>
+        public override void ChangeElement(int i, int j, T obj)
+        {
+            if(i!=j) throw new ArgumentException($"{nameof(i)} and {nameof(j)} must be the same in diagonal matrix");
+            base.ChangeElement(i, j, obj);
+        }
+    }
+}
